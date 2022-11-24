4fe64ea2300dd873a7e1c49110f978196c140776
eaPeter <peteremmanuel0544@gmail.com>
2022-11-24 20:10:51 +0000

    constants being defined

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+
+const kPadding = 20;
+
+const kBackgroundColor = Color(0x000451ae);
+const kTitleTextColor = Color(0x00003eac);
+const kBodyTextColor = Color(0x00000206);
+const kTextLightColor = Color(0x004d505e);
