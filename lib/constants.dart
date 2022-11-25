8443e37d593e71a2a4cc5df399aab4ac19e90453
eaPeter <peteremmanuel0544@gmail.com>
2022-11-25 13:27:05 +0000

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,43 @@
-import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
 
-const kPadding = 20;
+//padding
+const kPadding24 = 24;
+const kPadding20 = 20;
+const kPadding16 = 16;
+const kPadding12 = 12;
 
+//colors
 const kBackgroundColor = Color(0x000451ae);
 const kTitleTextColor = Color(0x00003eac);
 const kBodyTextColor = Color(0x00000206);
 const kTextLightColor = Color(0x004d505e);
+const kPrimaryColor = Color(0x000451ae);
+const kBlueColor = Color(0x000028B6);
+const kRedColor = Color(0x00de0000);
+final kShadowColor = const Color(0x000451ae).withOpacity(0.15);
+
+//text style
+const kHeadingLargeStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+const kHeadingMainStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+const kTopicStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+const kSubTopicStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
+
+const kOthersStyle = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w500,
+);
