0f31cfd90565609a9a2e8d4b18b73a9867ce180e
eaPeter <peteremmanuel0544@gmail.com>
2022-12-05 15:40:00 +0000

    Update notifications.dart

--- lib/screens/notifications/notifications.dart
+++ lib/screens/notifications/notifications.dart
@@ -11,45 +11,43 @@ class Notifications extends StatelessWidget {
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: kPadding20),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: kPadding16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                BackBtn(navigate: '/learn'),
-                Text(
-                  'Notifications',
-                  style: kTxtHeadingLargeStyle,
-                ),
-                SizedBox(width: 45),
-              ],
-            ),
-            Column(
-              children: [
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/images/notifications/no_notifications.png',
-                        height: 300,
-                        width: 300,
-                      ),
-                      const SizedBox(height: kPadding24),
-                      const Text('No notifications yet!',
-                          style: kTxtHeadingMainStyle),
-                      const Text(
-                          'Retry later to check for any new notifications',
-                          style: kTxtSubTopicStyle),
-                    ],
+        child: SizedBox(
+          height: double.infinity,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: kPadding16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  BackBtn(navigate: '/learn'),
+                  Text(
+                    'Notifications',
+                    style: kTxtHeadingLargeStyle,
                   ),
+                  SizedBox(width: 45),
+                ],
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/notifications/no_notifications.png',
+                      height: 300,
+                      width: 300,
+                    ),
+                    const SizedBox(height: kPadding24),
+                    const Text('No notifications yet!',
+                        style: kTxtHeadingMainStyle),
+                    const Text('Retry later to check for any new notifications',
+                        style: kTxtSubTopicStyle),
+                  ],
                 ),
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
       )),
     );
