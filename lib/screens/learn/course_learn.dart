d37e3561df6a918bee73968e46ef95563a3171d6
eaPeter <peteremmanuel0544@gmail.com>
2022-12-12 15:27:08 +0000

    Create course_learn.dart

---
+++ lib/screens/learn/course_learn.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CourseLearn extends StatelessWidget {
+  const CourseLearn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
