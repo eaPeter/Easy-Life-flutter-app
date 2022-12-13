4dddae966039be68412955cbb5d9204fbe6af840
eaPeter <peteremmanuel0544@gmail.com>
2022-12-13 17:03:43 +0000

    Create like_icon.dart

---
+++ lib/components/like_icon.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LikeBtn extends StatelessWidget {
+  const LikeBtn({super.key, this.isLiked = false});
+
+  final bool isLiked;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
