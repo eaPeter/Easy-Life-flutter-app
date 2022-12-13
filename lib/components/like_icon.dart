ace236282a9e3d2e7f1acb107573edc44ada47ca
eaPeter <peteremmanuel0544@gmail.com>
2022-12-13 17:25:51 +0000

    Update like_icon.dart

--- lib/components/like_icon.dart
+++ lib/components/like_icon.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
-class LikeBtn extends StatelessWidget {
+import '../constants.dart';
+
+class LikeBtn extends StatefulWidget {
   const LikeBtn({super.key, this.isLiked = false});
 
   final bool isLiked;
 
   @override
+  State<LikeBtn> createState() => _LikeBtnState();
+}
+
+class _LikeBtnState extends State<LikeBtn> {
+  late bool _isLiked = widget.isLiked;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 32,
+      width: 32,
+      decoration: BoxDecoration(
+          border: Border.all(color: kColorTitleText.withOpacity(0.15)),
+          shape: BoxShape.circle),
+      child: IconButton(
+        onPressed: () {
+          setState(() {
+            _isLiked = !_isLiked;
+          });
+        },
+        icon: Icon(
+          widget.isLiked ? Icons.favorite : Icons.favorite_border,
+          size: 20,
+          color: widget.isLiked ? kColorPrimary : kColorTextLight,
+        ),
+      ),
+    );
   }
 }
