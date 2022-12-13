a726f6f891d369aac10b4ae99955482349bc9340
eaPeter <peteremmanuel0544@gmail.com>
2022-12-13 14:59:07 +0000

    Update detailed_learn_page.dart

--- lib/screens/learn/detailed_learn_page.dart
+++ lib/screens/learn/detailed_learn_page.dart
@@ -13,113 +13,122 @@ class DetailedLearnPage extends StatelessWidget {
     double pageWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              alignment: Alignment.topLeft,
-              height: 295,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                      'assets/images/learn/Machine_learning_large.jpg'),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                height: 295,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                        'assets/images/learn/Machine_learning_large.jpg'),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(kPadding20),
+                  child: BackBtn(
+                    navigate: '/learn',
+                    whiteColorBool: true,
+                  ),
                 ),
               ),
-              child: const Padding(
-                padding: EdgeInsets.all(kPadding20),
-                child: BackBtn(
-                  navigate: '/learn',
-                  whiteColorBool: true,
-                ),
-              ),
-            ),
-            const SizedBox(height: kPadding16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: kPadding20),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: pageWidth * 0.8,
-                        child: const Text(
-                          "Machine Learning - Foundational Guide",
-                          style: kTxtHeadingMainStyle,
+              const SizedBox(height: kPadding16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: kPadding20),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: pageWidth * 0.8,
+                          child: const Text(
+                            "Machine Learning - Foundational Guide",
+                            style: kTxtHeadingMainStyle,
+                          ),
                         ),
-                      ),
-                      const Spacer(),
-                      Container(
-                        height: 32,
-                        width: 32,
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: kColorTitleText.withOpacity(0.15)),
-                            shape: BoxShape.circle),
-                        child: const Icon(Icons.favorite_border, size: 20),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: kPadding12),
-                  Row(
-                    children: [
-                      const DetailedPageSubTitle(
-                          svgPic: 'assets/svg/gamepad.svg',
-                          text: '130 playing'),
-                      Container(
-                        padding:
-                            const EdgeInsets.symmetric(horizontal: kPadding12),
-                        child: const Text('|',
-                            style: TextStyle(
-                                color: Color.fromRGBO(0, 0, 0, 0.45))),
-                      ),
-                      const DetailedPageSubTitle(
-                          svgPic: 'assets/svg/rank.svg', text: '#7 Ranked'),
-                      Container(
-                        padding:
-                            const EdgeInsets.symmetric(horizontal: kPadding12),
-                        child: const Text('|',
-                            style: TextStyle(
-                                color: Color.fromRGBO(0, 0, 0, 0.45))),
-                      ),
-                      const DetailedPageSubTitle(
-                          svgPic: 'assets/svg/checkBox_quizzes_taken.svg',
-                          text: '65 Quizzes'),
-                    ],
-                  ),
-                  const SizedBox(height: kPadding16),
-                  Text(
-                    'This course aims at testing your knowledge on Machine Leaning. The fundamentals of programming and internet security are prerequisites needed to be able to enjoy this game. Surprisingly, you can learn a lot more with playing game than reading when you are stressed. Rank up and receive rewards for your hardwork. Enjoy!!!',
-                    style: kTxtSubTopicStyle.copyWith(
-                        color: kColorTitleText.withOpacity(0.8)),
-                  ),
-                  const SizedBox(height: kPadding24),
-                  const DefaultButton(
-                      btnText: 'Learn', navigate: '/course_learn'),
-                  const SizedBox(height: kPadding24),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text('Other Courses', style: kTxtHeadingMainStyle),
-                      const Spacer(),
-                      GestureDetector(
-                        onTap: (() => Navigator.pushNamed(context, '/see_all')),
-                        child: const Text('See all', style: kTxtSubTopicStyle),
-                      ),
-                    ],
-                  ),
-                  GridView.builder(
-                      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                          maxCrossAxisExtent: 165,
-                          crossAxisSpacing: kPadding12),
-                      itemBuilder: (BuildContext context, index) {
-                        return Container();
-                      })
-                ],
+                        const Spacer(),
+                        Container(
+                          height: 32,
+                          width: 32,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: kColorTitleText.withOpacity(0.15)),
+                              shape: BoxShape.circle),
+                          child: const Icon(Icons.favorite_border, size: 20),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: kPadding12),
+                    Row(
+                      children: [
+                        const DetailedPageSubTitle(
+                            svgPic: 'assets/svg/gamepad.svg',
+                            text: '130 playing'),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: kPadding12),
+                          child: const Text('|',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(0, 0, 0, 0.45))),
+                        ),
+                        const DetailedPageSubTitle(
+                            svgPic: 'assets/svg/rank.svg', text: '#7 Ranked'),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: kPadding12),
+                          child: const Text('|',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(0, 0, 0, 0.45))),
+                        ),
+                        const DetailedPageSubTitle(
+                            svgPic: 'assets/svg/checkBox_quizzes_taken.svg',
+                            text: '65 Quizzes'),
+                      ],
+                    ),
+                    const SizedBox(height: kPadding16),
+                    Text(
+                      'This course aims at testing your knowledge on Machine Leaning. The fundamentals of programming and internet security are prerequisites needed to be able to enjoy this game. Surprisingly, you can learn a lot more with playing game than reading when you are stressed. Rank up and receive rewards for your hardwork. Enjoy!!!',
+                      style: kTxtSubTopicStyle.copyWith(
+                          color: kColorTitleText.withOpacity(0.8)),
+                    ),
+                    const SizedBox(height: kPadding24),
+                    const DefaultButton(
+                        btnText: 'Learn', navigate: '/course_learn'),
+                    const SizedBox(height: kPadding24),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text('Other Courses',
+                            style: kTxtHeadingMainStyle),
+                        const Spacer(),
+                        GestureDetector(
+                          onTap: (() =>
+                              Navigator.pushNamed(context, '/see_all')),
+                          child:
+                              const Text('See all', style: kTxtSubTopicStyle),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 120,
+                      child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithMaxCrossAxisExtent(
+                                  maxCrossAxisExtent: 165,
+                                  crossAxisSpacing: kPadding12),
+                          itemBuilder: (BuildContext context, index) {
+                            return Container();
+                          }),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
