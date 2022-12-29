3b6826ade96c1d3a7e608af726f88b5a8439c907
eaPeter <peteremmanuel0544@gmail.com>
2022-12-29 17:36:35 +0000

    Update settings_page_component.dart

--- lib/components/settings_page_component.dart
+++ lib/components/settings_page_component.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class SettingsPageComponent extends StatelessWidget {
-  const SettingsPageComponent({super.key});
+  const SettingsPageComponent(
+      {super.key,
+      required this.icon,
+      required this.title,
+      required this.subTitle});
+
+  final Icon icon;
+  final String title;
+  final String subTitle;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Row(
+          children: [
+            icon,
+            const SizedBox(width: kPadding12),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: kTxtTopicStyle,
+                ),
+                const SizedBox(height: kPadding4),
+                Text(
+                  subTitle,
+                  style: kTxtOthersStyle,
+                ),
+              ],
+            )
+          ],
+        ),
+        const SizedBox(height: kPadding16),
+        const Divider(thickness: 2),
+      ],
+    );
   }
 }
