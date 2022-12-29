47b29d09f63cf3d43cc747030ea059bb7e50a26b
eaPeter <peteremmanuel0544@gmail.com>
2022-12-29 17:13:15 +0000

    Update settings_page.dart

---
+++ lib/screens/settings/settings_page.dart
@@ -0,0 +1,17 @@
+import 'package:easy_life/constants.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: kPadding20),
+        ),
+      ),
+    );
+  }
+}
