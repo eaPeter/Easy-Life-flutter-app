0973b972e4a0878abaf21fd3da14465f9020130c
eaPeter <peteremmanuel0544@gmail.com>
2022-12-29 16:49:14 +0000

    Update see_all.dart

--- lib/screens/see_all.dart
+++ lib/screens/see_all.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../components/icon_btn.dart';
+import '../constants.dart';
+
 class SeeAll extends StatelessWidget {
   const SeeAll({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'See All',
+          style: kTxtTopicStyle,
+        ),
+        centerTitle: true,
+        backgroundColor: kColorBackground,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: kColorTitleText),
+      ),
+    );
   }
 }
