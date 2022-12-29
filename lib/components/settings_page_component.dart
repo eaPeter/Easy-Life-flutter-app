f7f21629a888cdbfe4d9857fb306ecb2cd088c14
eaPeter <peteremmanuel0544@gmail.com>
2022-12-29 17:34:01 +0000

    Create settings_page_component.dart

---
+++ lib/components/settings_page_component.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingsPageComponent extends StatelessWidget {
+  const SettingsPageComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
