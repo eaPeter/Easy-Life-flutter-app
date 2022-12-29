e1630aadb16276b9aa5579c57a6ad295db61f031
eaPeter <peteremmanuel0544@gmail.com>
2022-12-29 17:03:18 +0000

    Update see_all.dart

--- lib/screens/see_all.dart
+++ lib/screens/see_all.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../components/like_icon.dart';
 import '../constants.dart';
 
 class SeeAll extends StatelessWidget {
-  const SeeAll({super.key});
+  SeeAll({super.key});
+
+  final ScrollController _controller = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +14,7 @@ class SeeAll extends StatelessWidget {
       backgroundColor: kColorBackground,
       appBar: AppBar(
         title: const Text(
-          'See All',
+          'Accounting',
           style: kTxtTopicStyle,
         ),
         centerTitle: true,
@@ -21,9 +24,58 @@ class SeeAll extends StatelessWidget {
       ),
       body: SafeArea(
           child: Padding(
-        padding: EdgeInsets.symmetric(kPadding20),
+        padding: const EdgeInsets.symmetric(horizontal: kPadding20),
         child: SingleChildScrollView(
-          child: Column(),
+          child: Column(
+            children: [
+              GridView.builder(
+                  controller: _controller,
+                  shrinkWrap: true,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                      mainAxisSpacing: kPadding12,
+                      crossAxisSpacing: kPadding12,
+                      childAspectRatio: 0.96),
+                  itemCount: 10,
+                  itemBuilder: (BuildContext context, index) {
+                    return Container(
+                      padding: const EdgeInsets.only(left: 6, top: 6, right: 6),
+                      decoration: BoxDecoration(
+                        border:
+                            Border.all(color: kColorTitleText.withOpacity(0.3)),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(6)),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            height: 120,
+                            alignment: Alignment.topRight,
+                            padding: const EdgeInsets.all(kPadding12),
+                            decoration: const BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(3)),
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/learn/Machine_learning_large.jpg'),
+                                  fit: BoxFit.fill),
+                            ),
+                            child: const LikeBtn(),
+                          ),
+                          const SizedBox(height: kPadding8),
+                          const Text('Accounting', style: kTxtTopicStyle),
+                          const SizedBox(height: kPadding4),
+                          const Text('#7 Ranked',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(0, 0, 0, 0.6))),
+                        ],
+                      ),
+                    );
+                  }),
+            ],
+          ),
         ),
       )),
     );
