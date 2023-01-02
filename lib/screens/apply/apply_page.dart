8ed9ec29a33348856260f1cbaf1c4c5767586073
eaPeter <peteremmanuel0544@gmail.com>
2023-01-02 18:40:14 +0000

    Update apply_page.dart

--- lib/screens/apply/apply_page.dart
+++ lib/screens/apply/apply_page.dart
@@ -1,12 +1,30 @@
 import 'package:easy_life/constants.dart';
 import 'package:flutter/material.dart';
 
+import '../../components/icon_btn.dart';
+import '../drawer.dart';
+
 class ApplyPage extends StatelessWidget {
   const ApplyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kColorBackground,
+      drawer: const DrawerPage(),
+      appBar: AppBar(
+        backgroundColor: kColorBackground,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: kColorTitleText),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: kPadding20),
+            child: IconBtn(
+                iconChoice: Icon(Icons.notifications_outlined),
+                navigate: '/notifications'),
+          ),
+        ],
+      ),
       body: SafeArea(
           child:
               Padding(padding: EdgeInsets.symmetric(horizontal: kPadding20))),
