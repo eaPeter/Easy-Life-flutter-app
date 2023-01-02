7db82b8753de1324deb6a35f6bad87306342717c
eaPeter <peteremmanuel0544@gmail.com>
2023-01-02 18:38:02 +0000

    Create apply_page.dart

---
+++ lib/screens/apply/apply_page.dart
@@ -0,0 +1,15 @@
+import 'package:easy_life/constants.dart';
+import 'package:flutter/material.dart';
+
+class ApplyPage extends StatelessWidget {
+  const ApplyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child:
+              Padding(padding: EdgeInsets.symmetric(horizontal: kPadding20))),
+    );
+  }
+}
